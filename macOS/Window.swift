ee69708b0da6e99b2484dfc8004c10de7083ba32
vauxhall <zero.griffin@gmail.com>
2020-03-08 13:26:00 +0100

    Update window

--- macOS/Window.swift
+++ macOS/Window.swift
@@ -4,8 +4,8 @@ import Combine
 
 final class Window: NSWindow {
     init() {
-        super.init(contentRect: .init(x: 0, y: 0, width: 900, height: 600), styleMask: [.borderless, .miniaturizable, .resizable, .closable, .titled, .unifiedTitleAndToolbar, .fullSizeContentView], backing: .buffered, defer: false)
-        minSize = .init(width: 300, height: 200)
+        super.init(contentRect: .init(x: 0, y: 0, width: 400, height: 600), styleMask: [.borderless, .miniaturizable, .resizable, .closable, .titled, .unifiedTitleAndToolbar, .fullSizeContentView], backing: .buffered, defer: false)
+        minSize = .init(width: 200, height: 200)
         center()
         titlebarAppearsTransparent = true
         titleVisibility = .hidden
@@ -18,19 +18,30 @@ final class Window: NSWindow {
         blur.translatesAutoresizingMaskIntoConstraints = false
         contentView!.addSubview(blur)
         
-        let title = Label("", .medium(12))
-        title.textColor = .secondaryLabelColor
+        let title = Label(.key("App.title"), .medium(14))
+        title.textColor = .headerTextColor
         contentView!.addSubview(title)
         
-        let list = Scroll()
-        contentView!.addSubview(list)
-        
-        let content = Scroll()
-        contentView!.addSubview(content)
+        let scroll = Scroll()
+        contentView!.addSubview(scroll)
         
         blur.topAnchor.constraint(equalTo: contentView!.topAnchor).isActive = true
         blur.leftAnchor.constraint(equalTo: contentView!.leftAnchor).isActive = true
         blur.rightAnchor.constraint(equalTo: contentView!.rightAnchor).isActive = true
         blur.heightAnchor.constraint(equalToConstant: 38).isActive = true
+        
+        title.leftAnchor.constraint(equalTo: contentView!.leftAnchor, constant: 80).isActive = true
+        title.centerYAnchor.constraint(equalTo: blur.centerYAnchor).isActive = true
+        
+        scroll.topAnchor.constraint(equalTo: blur.bottomAnchor, constant: 1).isActive = true
+        scroll.leftAnchor.constraint(equalTo: contentView!.leftAnchor, constant: 1).isActive = true
+        scroll.rightAnchor.constraint(equalTo: contentView!.rightAnchor, constant: -1).isActive = true
+        scroll.bottomAnchor.constraint(equalTo: contentView!.bottomAnchor, constant: -1).isActive = true
+        scroll.right.constraint(equalTo: scroll.rightAnchor).isActive = true
+        scroll.bottom.constraint(greaterThanOrEqualTo: scroll.bottomAnchor).isActive = true
+    }
+    
+    override func close() {
+        NSApp.terminate(nil)
     }
 }
