951c6a25d04884dd848ceb0137fac34965d6abb0
vauxhall <zero.griffin@gmail.com>
2020-03-16 07:26:01 +0100

    Configure project

--- iOS/App.swift
+++ iOS/App.swift
@@ -1,23 +1,5 @@
 import UIKit
 
-@UIApplicationMain final class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_: UIApplication, configurationForConnecting: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return .init(name: "Default Configuration", sessionRole: configurationForConnecting.role)
-    }
-}
-
-import UIKit
-import SwiftUI
-
-final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    var window: UIWindow?
+@UIApplicationMain final class App: NSObject, UIApplicationDelegate {
     
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: List())
-            self.window = window
-            window.makeKeyAndVisible()
-        }
-    }
 }
