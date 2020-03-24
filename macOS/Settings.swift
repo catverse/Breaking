5bdd58be0c82fc4c2a18b507dd08bdb83e1670f4
vauxhall <zero.griffin@gmail.com>
2020-03-24 08:26:01 +0100

    Update settings

--- macOS/Settings.swift
+++ macOS/Settings.swift
@@ -11,6 +11,9 @@ final class Settings: NSWindow {
         collectionBehavior = .fullScreenNone
         isReleasedWhenClosed = false
         
+        let title = Label(.key("Settings.title"), .medium(12))
+        contentView!.addSubview(title)
+        
         let filterTitle = Label(.key("Settings.filter"), .light(14))
         filterTitle.textColor = .secondaryLabelColor
         contentView!.addSubview(filterTitle)
@@ -28,11 +31,38 @@ final class Settings: NSWindow {
         default: filter.selectItem(at: 0)
         }
         
-        filterTitle.leftAnchor.constraint(equalTo: contentView!.leftAnchor, constant: 60).isActive = true
+        let downloadTitle = Label(.key("Settings.reload"), .light(14))
+        downloadTitle.textColor = .secondaryLabelColor
+        contentView!.addSubview(downloadTitle)
+        
+        let download = NSPopUpButton(frame: .zero)
+        download.translatesAutoresizingMaskIntoConstraints = false
+        download.addItems(withTitles: [.key("Reload.5"), .key("Reload.15"), .key("Reload.30"), .key("Reload.60")])
+        download.target = self
+        download.action = #selector(self.download(_:))
+        contentView!.addSubview(download)
+        
+        switch news.preferences.refresh {
+        case 15: download.selectItem(at: 1)
+        case 30: download.selectItem(at: 2)
+        case 60: download.selectItem(at: 3)
+        default: download.selectItem(at: 0)
+        }
+        
+        title.centerYAnchor.constraint(equalTo: contentView!.topAnchor, constant: 19).isActive = true
+        title.centerXAnchor.constraint(equalTo: contentView!.centerXAnchor).isActive = true
+        
+        filterTitle.rightAnchor.constraint(equalTo: filter.leftAnchor, constant: -10).isActive = true
         filterTitle.centerYAnchor.constraint(equalTo: filter.centerYAnchor).isActive = true
         
         filter.topAnchor.constraint(equalTo: contentView!.topAnchor, constant: 60).isActive = true
-        filter.leftAnchor.constraint(equalTo: filterTitle.rightAnchor, constant: 20).isActive = true
+        filter.leftAnchor.constraint(equalTo: contentView!.leftAnchor, constant: 160).isActive = true
+        
+        downloadTitle.rightAnchor.constraint(equalTo: download.leftAnchor, constant: -10).isActive = true
+        downloadTitle.centerYAnchor.constraint(equalTo: download.centerYAnchor).isActive = true
+        
+        download.topAnchor.constraint(equalTo: filter.bottomAnchor, constant: 30).isActive = true
+        download.leftAnchor.constraint(equalTo: contentView!.leftAnchor, constant: 160).isActive = true
     }
     
     @objc private func filter(_ button: NSPopUpButton) {
@@ -43,5 +73,15 @@ final class Settings: NSWindow {
         }
         news.balam.update(news.preferences)
         news.reload()
+    }
+    
+    @objc private func download(_ button: NSPopUpButton) {
+        switch button.indexOfSelectedItem {
+        case 1: news.preferences.refresh = 15
+        case 2: news.preferences.refresh = 30
+        case 3: news.preferences.refresh = 60
+        default: news.preferences.refresh = 5
+        }
+        news.balam.update(news.preferences)
     }
 }
